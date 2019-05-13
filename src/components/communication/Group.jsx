fa79c7919b4295b0b239c60ed9141bc65d0b3fc6
907004362cz <2573433803@qq.com>
2019-05-13 23:43:47 +0800

    打完游戏了

--- src/components/communication/Group.jsx
+++ src/components/communication/Group.jsx
@@ -1,19 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 
-import { Button } from 'antd' 
+import { Button,Input,Table } from 'antd'
 
 import styled from 'styled-components'
 import color from '../../utils/color'
 
-
-
-function Group({username, userphone, handeleClick}) {
+function Group({tableColumns,tableData,rowSelection}) {
   const GroupBox = styled.div `
+    .search-wrap {
+      width: 100%;
+      height: 79px;
+      line-height: 79px;
+      background-color: rgba(229, 229, 229, 1);
+      padding: 0 26px;
+      .search-tit {
+        font-size: 16px;
+      }
+    }
+    .btn-wrap {
+      margin-top: 14px;
+      margin-bottom: 14px;
+      Button {
+        margin-right: 20px;
+      }
+    }
   `
   return (
     <GroupBox>
-       <Button type='primary'>GroupBox</Button>
+      <div className='search-wrap'>
+        <span className='search-tit'>快速搜索：</span>
+        <Input style={{width:'500px',marginLeft:'23px',marginRight:'35px'}}/>
+        <Button type="primary">搜索</Button>
+      </div>
+      <div className='btn-wrap'>
+        <Button type='primary'>新增</Button>
+        <Button type='primary'>修改</Button>
+        <Button type='primary'>一键拨打</Button>
+        <Button type='danger'>删除</Button>
+      </div>
+      <div>
+        <Table bordered rowSelection= {rowSelection} columns={tableColumns} dataSource={tableData} />
+      </div>
     </GroupBox>
   );
 }
--- src/components/duty/DutyManagement.jsx
+++ src/components/duty/DutyManagement.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 
-import { Button } from 'antd' 
+import { Button,Input } from 'antd'
 
 import styled from 'styled-components'
 import color from '../../utils/color'
 
 
 function DutyManagement({username, userphone, handeleClick}) {
-  const DutyManagementBox = styled.div `
-  `
-  return (
-    <DutyManagementBox>
-       <Button type='primary'>DutyManagementBox</Button>
-    </DutyManagementBox>
-  );
+const DutyManagementBox = styled.div `
+`
+return (
+  <DutyManagementBox>
+    <Button type='primary'>DutyManagementBox</Button>
+  </DutyManagementBox>
+);
 }
 DutyManagement.propTypes = {
-  // menuData: PropTypes.array.isRequired,
+// menuData: PropTypes.array.isRequired,
 }
 export default DutyManagement;
