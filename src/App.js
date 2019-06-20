203acd55ba93febb657e73caa0979327db11a384
zhangwangde <2573433803@qq.com>
2019-06-20 10:46:03 +0800

    1

--- src/App.js
+++ src/App.js
@@ -25,8 +25,6 @@ import CountHandleCase from './containers/count/CountHandleCase'
 import CountSignCase from './containers/count/CountSignCase'
 // 值班
 import DutyCase from './containers/menu/DutyCase'
-// import DutyManagementCase from './containers/duty/DutyManagementCase'
-// import DutyPlanCase from './containers/duty/DutyPlanCase'
 // 应急处置
 import ClassicCase from './containers/emergency/ClassicCase'
 import EvaluationCase from './containers/emergency/EvaluationCase'
@@ -139,8 +137,6 @@ function App({history}) {
                   <Route path='/emergency_handle' component={HandleCase}></Route>
                   <Route path='/emergency_evaluation' component={EvaluationCase}></Route>
                   <Route path='/emergency_classic' component={ClassicCase}></Route>
-                  {/* <Route path='/attendance_plan' component={DutyPlanCase}></Route>
-                  <Route path='/attendance_management' component={DutyManagementCase}></Route> */}
                   <Route path='/attendance' component={DutyCase}></Route>
                   <Route path='/communication_group' component={GroupCase}></Route>
                   <Route path='/communication_contact' component={ContactCase}></Route>
