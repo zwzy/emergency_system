74097203b10eeb0a4384fe4308654e83172ab0cd
18755489161@163.com <18755489161@163.com>
2019-07-17 20:10:02 +0800

    111

--- src/utils/getUrl.js
+++ src/utils/getUrl.js
@@ -1,4 +1,4 @@
-export const baseCallUrl = 'http://118.190.205.139:8051/sys/call'
-// export const baseCallUrl = 'http://10.131.172.87:8080/emergent/sys/call'
-export const baseUserUrl = 'http://118.190.205.139:8051/sys/user'
-// export const baseUserUrl = 'http://10.131.172.87:8080/emergent/sys/user'
+// export const baseCallUrl = 'http://118.190.205.139:8051/sys/call'
+export const baseCallUrl = 'http://10.131.172.87:8080/emergent/sys/call'
+// export const baseUserUrl = 'http://118.190.205.139:8051/sys/user'
+export const baseUserUrl = 'http://10.131.172.87:8080/emergent/sys/user'
