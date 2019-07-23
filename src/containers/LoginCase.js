8b020704f9fe50c592a9d8b5fc67f863fb47327f
18755489161@163.com <18755489161@163.com>
2019-07-23 22:46:08 +0800

    111

--- src/containers/LoginCase.js
+++ src/containers/LoginCase.js
@@ -50,16 +50,16 @@ export class LoginCase extends Component {
       return
     }
     try {
-      // const {data} = await login({workno, password})
-      const data = {
-        code: 0,
-        content: {
-          deptName: '上海',
-          roleList: [{id:1,roleName:'组长'}, {id:2,roleName:'普通员工'}],
-          extNum: 1001,
-          userName: '张太胖'
-        }
-      }
+      const {data} = await login({workno, password})
+      // const data = {
+      //   code: 0,
+      //   content: {
+      //     deptName: '上海',
+      //     roleList: [{id:1,roleName:'组长'}, {id:2,roleName:'普通员工'}],
+      //     extNum: 1001,
+      //     userName: '张太胖'
+      //   }
+      // }
       if(data.code === 0) {
         const {roleList, extNum, userName, deptName} = data.content
         // 保存状态
